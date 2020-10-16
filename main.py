bc87c5b4f065c9efdaf95efacb0bba338e76cd88
PhileasL <phileas.lambert@gmail.com>
2020-10-16 18:24:59 +0200

    pathExist function

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class State:
 ######################
 
 def getEssentialsPositions(array):
-    player, boxes, walls = [0,0], [], []
+    player, boxes, walls = [0,0], [], [[' ' for _ in range(len(array[0]))] for _ in range(len(array))]
     for i in range(len(array)):
         for j in range(len(array[0])):
             if array[i][j] == '@':
@@ -49,7 +49,7 @@ def getEssentialsPositions(array):
             if array[i][j] == '$':
                 boxes.append([i,j])
             if array[i][j] == "#":
-                walls.append([i, j])
+                walls[i][j] = '#'
     return player, boxes, walls
 
 def openFile(path):
@@ -76,6 +76,43 @@ def printBeautifulPath(array):
 		print()
 	print()
 
+def removeSides(grid): #maybe not necessary
+    newGrid = grid.copy()
+    newGrid.remove(grid[0])
+    newGrid.remove(grid[len(grid)-1])
+    for i in range(len(grid)-2):
+        newGrid[i].pop(0)
+        newGrid[i].pop(len(grid)+1)
+    return newGrid
+
+directions = [ [0, -1], [0, 1], [-1, 0], [1, 0] ]
+
+def pathExists(actualGrid, start, end, boxes):
+    grid = actualGrid.copy()
+    for i in boxes:
+        grid[i[0]][i[1]] = '#'
+    printBeautifulPath(grid)
+    visited = [ [0 for j in range(0, len(grid[0]))] for i in range(0, len(grid)) ]
+    ok = pathExistsDFS(grid, start, end, visited)
+    return ok
+
+def pathExistsDFS(grid, start, end, visited):
+    for d in directions:
+        i = start[0] + d[0]
+        j = start[1] + d[1]
+        next = [i, j]
+        if i == end[0] and j == end[1]:
+            printBeautifulPath(visited)
+            return True
+        if grid[i][j] == ' ' and not visited[i][j]:
+            visited[i][j] = 1
+            exists = pathExistsDFS(grid, next, end, visited)
+            if exists:
+                printBeautifulPath(visited)
+                return True
+    printBeautifulPath(visited)
+    return False
+
 #####################
 # Launch the search #
 #####################
@@ -85,6 +122,8 @@ grid = openFile(sys.argv[1])
 printBeautifulPath(grid)
 
 player, boxes, walls = getEssentialsPositions(grid)
-
+printBeautifulPath(walls)
 state = State(player, boxes)
-print(state.__hash__())
+#sidesRemoved = removeSides(grid)
+print(boxes)
+print(pathExists(walls, [4,2], [4,9], boxes))
